3cda904a549ea9bcd90a13b92b74c922b80d5a09
Chinnu Ajith <chin3nu@gmail.com>
2020-06-14 15:33:18 -0400

    first commit

---
+++ my_app.py
@@ -0,0 +1,8 @@
+import yfinance as yf
+import streamlit as st
+st.write()
+tickerSymbol = 'GOOGL'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
